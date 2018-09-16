b59e7f4369ebe191c3efd899ecdef85424c35dce
gameaddict12345 <39905348+gameaddict12345@users.noreply.github.com>
2018-09-16 14:38:06 +0800

    UI Part 1

--- ATM System/PIN.Designer.cs
+++ ATM System/PIN.Designer.cs
@@ -47,7 +47,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(40, 12);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(470, 116);
@@ -56,144 +56,170 @@
             // 
             // button1
             // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(40, 134);
+            this.button1.Image = global::ATM_System.Properties.Resources._1;
+            this.button1.Location = new System.Drawing.Point(37, 134);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 82);
             this.button1.TabIndex = 1;
-            this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
-            this.button2.Location = new System.Drawing.Point(172, 134);
+            this.button2.Image = global::ATM_System.Properties.Resources._2;
+            this.button2.Location = new System.Drawing.Point(131, 134);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 82);
             this.button2.TabIndex = 2;
-            this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(304, 134);
+            this.button3.Image = global::ATM_System.Properties.Resources._3;
+            this.button3.Location = new System.Drawing.Point(225, 134);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(88, 82);
             this.button3.TabIndex = 3;
-            this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(40, 232);
+            this.button4.Image = global::ATM_System.Properties.Resources._4;
+            this.button4.Location = new System.Drawing.Point(37, 222);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(88, 82);
             this.button4.TabIndex = 4;
-            this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(172, 232);
+            this.button5.Image = global::ATM_System.Properties.Resources._5;
+            this.button5.Location = new System.Drawing.Point(131, 222);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(88, 82);
             this.button5.TabIndex = 5;
-            this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(304, 232);
+            this.button6.Image = global::ATM_System.Properties.Resources._6;
+            this.button6.Location = new System.Drawing.Point(225, 222);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(88, 82);
             this.button6.TabIndex = 6;
-            this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(40, 334);
+            this.button7.Image = global::ATM_System.Properties.Resources._7;
+            this.button7.Location = new System.Drawing.Point(37, 316);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(88, 82);
             this.button7.TabIndex = 7;
-            this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(172, 334);
+            this.button8.Image = global::ATM_System.Properties.Resources._8;
+            this.button8.Location = new System.Drawing.Point(131, 316);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(88, 82);
             this.button8.TabIndex = 8;
-            this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(304, 334);
+            this.button9.Image = global::ATM_System.Properties.Resources._9;
+            this.button9.Location = new System.Drawing.Point(225, 316);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(88, 82);
             this.button9.TabIndex = 9;
-            this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // buttonOK
             // 
+            this.buttonOK.FlatAppearance.BorderSize = 0;
+            this.buttonOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonOK.Location = new System.Drawing.Point(422, 134);
+            this.buttonOK.Image = global::ATM_System.Properties.Resources.O;
+            this.buttonOK.Location = new System.Drawing.Point(365, 134);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(88, 82);
             this.buttonOK.TabIndex = 10;
-            this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
             this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
             // 
             // buttonCLEAR
             // 
+            this.buttonCLEAR.FlatAppearance.BorderSize = 0;
+            this.buttonCLEAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonCLEAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCLEAR.Location = new System.Drawing.Point(422, 232);
+            this.buttonCLEAR.Image = global::ATM_System.Properties.Resources.X;
+            this.buttonCLEAR.Location = new System.Drawing.Point(365, 222);
             this.buttonCLEAR.Name = "buttonCLEAR";
             this.buttonCLEAR.Size = new System.Drawing.Size(88, 82);
             this.buttonCLEAR.TabIndex = 11;
-            this.buttonCLEAR.Text = "CLEAR";
             this.buttonCLEAR.UseVisualStyleBackColor = true;
             this.buttonCLEAR.Click += new System.EventHandler(this.buttonCLEAR_Click);
             // 
             // buttonCANCEL
             // 
+            this.buttonCANCEL.FlatAppearance.BorderSize = 0;
+            this.buttonCANCEL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonCANCEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCANCEL.Location = new System.Drawing.Point(422, 334);
+            this.buttonCANCEL.Image = global::ATM_System.Properties.Resources.cancel;
+            this.buttonCANCEL.Location = new System.Drawing.Point(365, 316);
             this.buttonCANCEL.Name = "buttonCANCEL";
             this.buttonCANCEL.Size = new System.Drawing.Size(88, 82);
             this.buttonCANCEL.TabIndex = 12;
-            this.buttonCANCEL.Text = "CANCEL";
             this.buttonCANCEL.UseVisualStyleBackColor = true;
             this.buttonCANCEL.Click += new System.EventHandler(this.buttonCANCEL_Click);
             // 
             // button0
             // 
+            this.button0.FlatAppearance.BorderSize = 0;
+            this.button0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button0.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button0.Location = new System.Drawing.Point(172, 423);
+            this.button0.Image = global::ATM_System.Properties.Resources._0;
+            this.button0.Location = new System.Drawing.Point(131, 404);
             this.button0.Name = "button0";
             this.button0.Size = new System.Drawing.Size(88, 82);
             this.button0.TabIndex = 13;
-            this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = true;
             this.button0.Click += new System.EventHandler(this.button0_Click);
             // 
@@ -201,7 +227,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 517);
+            this.BackgroundImage = global::ATM_System.Properties.Resources.PIN_BG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(492, 497);
             this.Controls.Add(this.button0);
             this.Controls.Add(this.buttonCANCEL);
             this.Controls.Add(this.buttonCLEAR);
